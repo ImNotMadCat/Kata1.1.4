147797cd0ccb8aa83915f48748ed7bb3ca0c02ec
Imnotmadcat <Imnotmadcat@gmail.com>
2022-07-10 11:09:15 +0600

    First commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,26 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+    public static void main(String[] args) throws IOException {
+        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+
+        userDao.createUsersTable();
+        userDao.saveUser("Neil","Alishev",(byte) 30);
+        userDao.saveUser("Zaur","Tregulov",(byte) 41);
+        userDao.saveUser("Joe","Biden",(byte) 75);
+        userDao.saveUser("Bruce","Banner",(byte) 34);
+        System.out.println(userDao.getAllUsers());
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
+        Util.closeConnection();
+
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,36 @@
 package jm.task.core.jdbc.util;
 
+import com.mysql.cj.jdbc.Driver;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static Connection connection;
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Imthemadcat19";
+    private static final String URL = "jdbc:mysql://localhost:3306/people";
+    public static Connection getConnection() {
+        try {
+            Driver driver = new Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            System.out.println("Соединение успешно");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+    public static void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
